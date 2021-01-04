8ec58e897513984907d67932c00ee098f04a3475
Alvaro <alvinweeb@gmail.com>
2021-01-03 21:02:58 -0300

    Funções

---
+++ js/functions.js
@@ -0,0 +1,17 @@
+// Criar um app de frases motivacionais
+
+// declaration - declaração da função
+// function statement
+function createPhrases() {
+    console.log('Estudar é muito bom');
+    console.log('Paciência e persistência');
+    console.log('Revisão é mãe do aprendizado');
+}
+
+// ecexutar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+createPhrases()
+
+// fim da execução 
+
